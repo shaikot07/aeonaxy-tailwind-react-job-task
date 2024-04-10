8fc3f5155a43afbe4402384ead2cec050928f7c1
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-10 23:42:49 +0600

    added custum button

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -14,29 +14,19 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            
+                            {link}
                         </ul>
                     </div>
                     {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
                     <img src="https://i.ibb.co/HxkFhsJ/logo.png" alt="" />
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
+                    {link}
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="my-btn">Button</a>
+                    <a className="btn ml-1">Get Start</a>
+                    
                 </div>
             </div>
         </div>
