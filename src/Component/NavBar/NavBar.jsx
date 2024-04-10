e7173d4aecb149aed0dd82dc0449bf8a7bb19774
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-10 23:20:05 +0600

    make link for navbar

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+    const link = <>
+        <li> <Link className="rounded-none text-slate-50 font-bold border-b-0 hover:!text-[#A855F7] hover:border-[#A855F7] hover:border-b-2 transition duration-300 focus:!text-white" to={'/'}>Home</Link> </li>
+    </>
     return (
         <div>
             <div className="navbar bg-red-400">
@@ -10,15 +14,7 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            
                         </ul>
                     </div>
                     {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
--- src/LayOut/Layout.jsx
+++ src/LayOut/Layout.jsx
@@ -4,11 +4,13 @@ import NavBar from '../Component/NavBar/NavBar';
 
 const Layout = () => {
     return (
-        <div>
-            <NavBar></NavBar>
-            <div>
-                {/* <Outlet></Outlet> */}
-                <Outlet></Outlet>
+        <div className=''>
+            <div className='w-full'>
+                <NavBar></NavBar>
+                <div>
+                    {/* <Outlet></Outlet> */}
+                    <Outlet></Outlet>
+                </div>
             </div>
         </div>
     );
