a870a849f18a6455ba2049d46a79c9cff5d98f9d
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-14 22:48:43 +0600

    added main color

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,6 +6,7 @@ const Banner = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-xl rounded-lg shadow-2xl" />
                     <div>
+                        <p className="py-4 text-1xl font-semibold text-[#75a7e5]">CALENDLY FEATURES</p>
                         <h1 className="text-5xl font-bold">Box Office News!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <button className="btn btn-primary">Get Started</button>
